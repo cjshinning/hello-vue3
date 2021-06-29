d43a6e4b7bafe6f21f91c261f637518426b240eb
cjshinning <784319417@qq.com>
2021-06-29 15:52:12 +0800

    重学vue3学习-生命周期

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,51 @@
 <template>
-  <p>名： {{user.firstName}}</p>
-  <p>性： {{user.lastName}}</p>
-  <p>全名： {{fullName}}</p>
+  <h3>vue3 生命周期</h3>
+  <p>{{counter}}</p>
+  <button @click="updateCounter">增加</button>
 </template>
 
 <script>
-import {ref, reactive, watchEffect} from 'vue';
+import {ref, onMounted, onBeforeMount, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted} from 'vue';
 export default {
   setup(){
-    const user = reactive({
-      firstName: 'Jenny',
-      lastName: 'Chan'
-    });
-    const fullName = ref('');
-    watchEffect(() => {
-      fullName.value = user.firstName + '_' + user.lastName;
+    const counter = ref(0);
+    const updateCounter = () => {
+      counter.value++;
+    }
+    onBeforeMount(() => {
+      console.log('--onBeforeMount');
+    })
+    onMounted(() => {
+      console.log('--onMounted');
+    })
+    onBeforeUpdate(() => {
+      console.log('--onBeforeUpdate');
+    })
+    onUpdated(() => {
+      console.log('--onUpdated');
+    })
+    onBeforeUnmount(() => {
+      console.log('--onBeforeUnmount');
+    })
+    onUnmounted(() => {
+      console.log('--onUnmounted');
     })
     return {
-      user,
-      fullName
+      counter,
+      updateCounter
     }
+  },
+  created(){
+    console.log('created');
+  },
+  beforeCreate(){
+    console.log('beforeCreate');
+  },
+  beforeMount(){
+    console.log('beforeMount');
+  },
+  mounted(){
+    console.log('mounted');
   }
 }
 </script>
