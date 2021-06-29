7f411d7eb40eb4b0a238fd7aff0461514215d10a
cjshinning <784319417@qq.com>
2021-06-29 14:56:55 +0800

    重学vue3学习-reactive

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
-  <div>{{counter}}</div>
+  <p>年龄： {{user.name}}</p>
+  <p>性别： {{user.age}}</p>
+  <p>其他： {{user.other}}</p>
+  <button @click="updateUser">更新</button>
 </template>
 
 <script>
-import {ref} from 'vue';
+import {reactive} from 'vue';
 export default {
   setup(){
-    const counter = ref(0);
+    const user = reactive({
+      name: 'Jenny',
+      age: 18,
+      other: {
+        country: 'China',
+        hobbies: ['coding', 'singing']
+      }
+    });
+    const updateUser = () => {
+      user.name = 'Jane';
+      user.age = 20;
+      user.other.hobbies[1] = 'drawing';
+    }
     return {
-      counter
+      user,
+      updateUser
     }
   }
 }
